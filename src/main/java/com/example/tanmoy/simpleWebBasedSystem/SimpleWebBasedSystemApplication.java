d667050ed2a7b68985eb06145ec2ddcbd472de6d
mandaltanmoy1938 <mandaltanmoy1938@gmail.com>
2017-07-01 23:41:42 +0600

    Starting..

---
+++ src/main/java/com/example/tanmoy/simpleWebBasedSystem/SimpleWebBasedSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.tanmoy.simpleWebBasedSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebBasedSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebBasedSystemApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/tanmoy/simpleWebBasedSystem/TaskEntity.java
@@ -0,0 +1,5 @@
+package com.example.tanmoy.simpleWebBasedSystem;
+
+public class TaskEntity {
+
+}
